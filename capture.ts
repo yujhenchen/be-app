bcd87b8ae02db59d1ac28e89f2f5544c5d4525da
yujhenchen <yujhen.chen93@gmail.com>
2024-09-03 20:43:56 +0800

    feat: add biome script and apply style fixes

--- capture.ts
+++ capture.ts
@@ -1,57 +1,60 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
-function parseURL(url: string) {
-    const decodedUrl = decodeURIComponent(url);
+async function parseURL(url: string) {
+	const decodedUrl = decodeURIComponent(url);
 
-    // Create a URL object
-    const urlObj = new URL(decodedUrl);
+	// Create a URL object
+	const urlObj = new URL(decodedUrl);
 
-    // Extract the 'json' parameter and decode it
-    const jsonParam = urlObj.searchParams.get('json');
-    let jsonObject: Record<string, string> = {};
-    if (jsonParam) {
-        try {
-            // Parse the JSON parameter
-            jsonObject = JSON.parse(jsonParam);
-            // console.log('Extracted JSON Object:', jsonObject);
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-        }
-    } else {
-        console.error('No JSON parameter found in the URL.');
-    }
+	// Extract the 'json' parameter and decode it
+	const jsonParam = urlObj.searchParams.get("json");
+	let jsonObject: Record<string, string> = {};
+	if (jsonParam) {
+		try {
+			// Parse the JSON parameter
+			jsonObject = await JSON.parse(jsonParam);
+			// console.log('Extracted JSON Object:', jsonObject);
+		} catch (error) {
+			console.error("Error parsing JSON:", error);
+		}
+	} else {
+		console.error("No JSON parameter found in the URL.");
+	}
 
-    // extract token
-    const token = urlObj.searchParams.get('token')
-    // console.log('token: ', token);
-    return {
-        jsonObject,
-        token
-    }
+	// extract token
+	const token = urlObj.searchParams.get("token");
+	console.log("token: ", token);
+	return {
+		jsonObject,
+		token,
+	};
 }
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-    // Set up request interception
-    await page.setRequestInterception(true);
-    page.on('request', request => {
-        // console.log('Captured Request:', request.url());
-        if (process.env.NO_EVENT_REQ_PART && request.url().includes(process.env.NO_EVENT_REQ_PART)) {
-            console.log(parseURL(request.url()));
-        }
-        request.continue();
-    });
+	// Set up request interception
+	await page.setRequestInterception(true);
+	page.on("request", async (request) => {
+		console.log("Captured Request:", request.url());
+		if (
+			process.env.NO_EVENT_REQ_PART &&
+			request.url().includes(process.env.NO_EVENT_REQ_PART)
+		) {
+			console.log(await parseURL(request.url()));
+		}
+		await request.continue();
+	});
 
-    // Navigate to the website
-    const url = process.env.NO_URL;
-    if (url) {
-        await page.goto(url);
+	// Navigate to the website
+	const url = process.env.NO_URL;
+	if (url) {
+		await page.goto(url);
 
-        setTimeout(async () => {
-            await browser.close();
-        }, 7000);
-    }
-    await browser.close();
+		setTimeout(async () => {
+			await browser.close();
+		}, 10000);
+	}
+	await browser.close();
 })();
