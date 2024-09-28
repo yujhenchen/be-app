ff31b98f56d360d42745abdd2e354f83aceb951c
yujhenchen <yujhen.chen93@gmail.com>
2024-09-28 16:24:26 +0800

    feat: serve htmx and static html files

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import config from "./src/config/config";
+import path from "path";
 
 const app = express();
 const port = 8080;
 
+app.use(express.static(config.viewsPath))
+
+// Serve htmx.js from node_modules
+app.get('/htmx.js', (req, res) => {
+	res.sendFile(path.join(__dirname, '.', 'node_modules', 'htmx.org', 'dist', 'htmx.js'));
+});
+
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	// res.send("Hello World!");
+	res.sendFile(path.join(config.viewsPath, "index.html"));
+});
+
+// TODO: remove this test use
+app.get('/data', (req, res) => {
+	res.send('<p>This is the fetched data from the server!</p>');
 });
 
 app.listen(port, () => {
